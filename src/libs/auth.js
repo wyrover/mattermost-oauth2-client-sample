516a594feef0e3059078435139f43a5e99a9bb87
wyrover <wyrover@gmail.com>
2021-12-07 17:38:10 +0800

    * src

---
+++ src/libs/auth.js
@@ -0,0 +1,49 @@
+/**
+ * Created by enahum on 6/13/16.
+ */
+
+import config from './config'
+import { Client4 as mattermost } from 'mattermost-redux/client'
+import passport from 'passport'
+import { Strategy as OAuth2Strategy } from 'passport-oauth2'
+
+var auth = {
+  url: null,
+  setUrl (mattermostUrl) {
+    this.url = mattermostUrl
+
+    const oauth2Strategy = new OAuth2Strategy({
+      authorizationURL: `${mattermostUrl}/oauth/authorize`,
+      tokenURL: `${mattermostUrl}/oauth/access_token`,
+      clientID: config.get('client_id'),
+      clientSecret: config.get('client_secret'),
+      callbackURL: `${config.get('client_url')}/oauth/callback`
+    }, (accessToken, refreshToken, profile, cb) => {
+      profile.accessToken = accessToken
+      profile.refreshToken = refreshToken
+      return cb(null, profile)
+    })
+
+    oauth2Strategy.userProfile = (accessToken, done) => {
+      mattermost.setUrl(auth.url)
+      mattermost.setToken(accessToken)
+      mattermost.getMe().then((data) => {
+        done(null, data)
+      }).catch((err) => {
+        done(err)
+      })
+    }
+
+    passport.use(oauth2Strategy)
+  }
+}
+
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.deserializeUser((user, done) => {
+  done(null, user)
+})
+
+module.exports = auth
---
+++ src/routes/index.js
@@ -0,0 +1,70 @@
+import auth from '../libs/auth'
+import config from '../libs/config'
+import express from 'express'
+import passport from 'passport'
+
+const router = express.Router()
+
+auth.setUrl(config.get('mattermost_url')) // only for testing purposes
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return res.render('index', { title: 'Mattermost OAuth 2.0 Client Sample App' })
+  }
+
+  return res.redirect('/user')
+})
+
+router.get('/login', passport.authenticate('oauth2'))
+
+router.get('/oauth/callback', (req, res, next) => {
+  passport.authenticate('oauth2', (err, user) => {
+    if (err) {
+      return next(err)
+    }
+    if (req.query.error) {
+      return res.render('oauth', {
+        title: 'Mattermost Sample App',
+        error: req.query.error
+      })
+    }
+    if (!user) {
+      return res.render('oauth', {
+        title: 'Mattermost Sample App',
+        error: 'User failed'
+      })
+    }
+
+    req.logIn(user, function (err) {
+      if (err) { return next(err) }
+
+      return res.render('oauth', {
+        title: 'Mattermost Sample App',
+        logged: true
+      })
+    })
+  })(req, res, next)
+})
+
+router.get('/user', (req, res) => {
+  if (req.isAuthenticated()) {
+    return res.render('user', {
+      title: 'Mattermost Sample App Authenticated',
+      user: req.user
+    })
+  }
+  return res.redirect('/')
+})
+
+router.post('/client', (req, res) => {
+  auth.setUrl(req.body.url)
+  return res.json(true)
+})
+
+router.get('/logout', (req, res) => {
+  req.logOut()
+  return res.redirect('/')
+})
+
+module.exports = router
