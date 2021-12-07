dd90831ef66ba8359e105a58819c6af72ad755d5
wyrover <wyrover@gmail.com>
2021-12-07 18:11:49 +0800

    + form-data

--- deploy_server.sh
+++ deploy_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avzh -e 'ssh -p XX' --progress --delete ./dist root@XXXXXX:/opt/mattermost_client/
+rsync -avzh -e 'ssh -p 29931' --progress --exclude-from='exclude-file.txt' ./dist root@144.34.243.83:/opt/mattermost_client/
--- deploy_server_mattermost-redux.sh
+++ deploy_server_mattermost-redux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avzh -e 'ssh -p XX' --progress --delete ./node_modules/mattermost-redux/ root@XXXXXX:/opt/mattermost_client/dist/node_modules/mattermost-redux/
+rsync -avzh -e 'ssh -p 29931' --progress --delete ./node_modules/mattermost-redux/ root@144.34.243.83:/opt/mattermost_client/dist/node_modules/mattermost-redux/
